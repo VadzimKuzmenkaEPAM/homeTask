a49469a9a9e0f93852cb35d4ad5e603f9a04173e
Vadzim Kuzmenka <Vadzim_Kuzmenka@epam.com>
2022-07-15 13:07:32 +0400

    Fixed code

--- src/nested/Car.java
+++ src/nested/Car.java
@@ -1,32 +1,72 @@
 package nested;
 
 public class Car {
-    String colour;
-    int doorCount;
-    Engine engine;
-    GasTank gasTank;
+    private String colour;
+    private static String carCondition;
+    private Engine engine;
+    private GasTank gasTank;
 
-    public Car(String colour, int doorCount, Engine engine, int value) {
+    public Car(String colour, String carCondition, Engine engine, int value) {
         this.colour = colour;
-        this.doorCount = doorCount;
+        Car.carCondition = carCondition;
         this.engine = engine;
         this.gasTank = this.new GasTank(value);
+    }
+
+    public String getColour() {
+        return colour;
+    }
+
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
+
+    public static String getCarCondition() {
+        return carCondition;
+    }
+
+    public static void setCarCondition(String carCondition) {
+        Car.carCondition = carCondition;
+    }
+
+    public Engine getEngine() {
+        return engine;
+    }
+
+    public void setEngine(Engine engine) {
+        this.engine = engine;
+    }
+
+    public GasTank getGasTank() {
+        return gasTank;
+    }
+
+    public void setGasTank(GasTank gasTank) {
+        this.gasTank = gasTank;
     }
 
     @Override
     public String toString() {
         return "Car{" +
                 "colour='" + colour + '\'' +
-                ", doorCount=" + doorCount +
+                ", carCondition=" + carCondition +
                 ", engine=" + engine +
                 ", gasTank=" + gasTank +
                 '}';
     }
 
     public static class Engine { // static Nested class
-        int horsePower;
+        private int horsePower;
 
         public Engine(int horsePower) {
+            this.horsePower = horsePower;
+        }
+
+        public int getHorsePower() {
+            return horsePower;
+        }
+
+        public void setHorsePower(int horsePower) {
             this.horsePower = horsePower;
         }
 
@@ -35,6 +75,10 @@ public class Car {
             return "My Engine{" +
                     "horsePower=" + horsePower +
                     '}';
+        }
+
+        public void printInfo() {
+            System.out.println("horsePower -> " + horsePower + " car condition ->" + carCondition); // we can have access only to static fields
         }
     }
 
@@ -53,14 +97,12 @@ public class Car {
         }
     }
 }
-    class Test {
-        public static void main(String[] args) {
-            Car.Engine engine = new Car.Engine(299);
-            Car car = new Car("Black", 2, engine, 200);
 
-
-
-            System.out.println(car);
-        }
+class Main {
+    public static void main(String[] args) {
+        Car.Engine engine = new Car.Engine(299);
+        Car car = new Car("Black ", "new", engine, 200);
+        engine.printInfo();
+        System.out.println(car);
     }
-
+}
